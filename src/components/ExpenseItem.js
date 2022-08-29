00e75a85ff129bca11d2f3a6fe88390f59b2953b
RJay Sharma <rsharma@beesolvertechnology.com>
2022-08-29 16:30:01 +0530

    worked on components composition.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-
-  const expenseDate = new Date(2022, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 299;
-  
+function ExpenseItem(props) {
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toDateString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
